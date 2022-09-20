d70e4b0a49a09af1228eff47bde4274ef315df92
osmancoskun <osman.coskun95@gmail.com>
2022-09-21 01:42:42 +0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import preprocess from "svelte-preprocess";
+
+export default defineConfig({
+  server: {
+    port: 5000,
+  },
+  plugins: [
+    svelte({
+      preprocess: [preprocess({
+        postcss: true
+      })],
+    }),
+  ],
+});
